a35a09800308dc96303a7813501c92e3260c92c0
minihorse5 <rimr52@inf.elte.hu>
2024-10-14 23:30:53 +0200

    added admin login/logout, about page

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,8 +1,10 @@
 from itertools import product
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import views
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     products = Product.objects.all()
@@ -10,4 +12,24 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in!")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error, please, try again!")
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out, bro!")
+    return redirect('home')
 
