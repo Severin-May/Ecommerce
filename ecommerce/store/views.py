3a4d8042b015fd3571ceff1934a5c93cc4b9fd75
minihorse5 <rimr52@inf.elte.hu>
2024-10-14 22:57:04 +0200

    refactored template, added sale conditional logic

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -7,3 +7,7 @@ from .models import Product
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products':products})
+
+def about(request):
+    return render(request, 'about.html', {})
+
