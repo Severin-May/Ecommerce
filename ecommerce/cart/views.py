dd8b622e0532287a701ecadfe45b94969f706c3e
minihorse5 <rimr52@inf.elte.hu>
2024-10-23 22:01:35 +0200

    added product add to cart functionality

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from django.http import JsonResponse
+
+from store.models import Product
+
 
 # Create your views here.
 def cart_summary(request):
     return render(request, 'cart_summary.html', {})
 
 def cart_add(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product)
+        response = JsonResponse({'Product Name: ': product.name})
+
+        return response
 
 def cart_delete(request):
     pass
