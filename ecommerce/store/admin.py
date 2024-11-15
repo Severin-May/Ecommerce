fec3ccf15663b9dcdd6ebb45d8bbf80b39d4974d
minihorse5 <rimr52@inf.elte.hu>
2024-11-15 16:58:07 +0100

    added user profile model

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
-
-from . models import Category, Product, Order, Customer
+from django.contrib.auth.models import User
+from .models import Category, Product, Order, Customer, Profile
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(Customer)
+admin.site.register(Profile)
 # Register your models here.
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email']
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 import datetime
 
 class Category(models.Model):
@@ -44,3 +46,25 @@ class Order(models.Model):
 
     def __str__(self):
         return f'{self.product} {self.quantity}'
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    modification_date = models.DateTimeField(User, auto_now=True)
+    phone = models.CharField(max_length=20, blank=True)
+    address1 = models.CharField(max_length=200, blank=True)
+    address2 = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=200, blank=True)
+    country = models.CharField(max_length=200, blank=True)
+    zip_code = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+def create_profile(sender, instance, created, **kwargs):
+
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+post_save.connect(create_profile, sender=User)
+
