cb004e29bef94b847b040759b8e60bac67ad34f2
minihorse5 <rimr52@inf.elte.hu>
2024-12-06 23:06:05 +0100

    added billing info, placing order logic, categories context processor

--- ecommerce/payment/forms.py
+++ ecommerce/payment/forms.py
@@ -15,3 +15,16 @@ class ShippingForm(forms.ModelForm):
         model = ShippingAddress
         fields = ('ship_full_name', 'ship_email', 'ship_address1', 'ship_address2', 'ship_city', 'ship_state', 'ship_country', 'ship_zipcode')
         exclude = ['user']
+
+
+class PaymentForm(forms.Form):
+    card_name = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'card name'}), required=True)
+    card_number = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'card number'}), required=True)
+    card_exp_date = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'card expiration date'}), required=True)
+    card_cvv = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'card cvv'}), required=True)
+    card_addr1 = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'card address 1'}), required=True)
+    card_addr2 = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'card address 1'}), required=True)
+    card_city = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'card city'}), required=True)
+    card_state = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'card state'}), required=True)
+    card_country = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'card country'}), required=True)
+    card_zip = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'card zipcode'}), required=True)
--- ecommerce/payment/views.py
+++ ecommerce/payment/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
 from cart.cart import Cart
-from payment.forms import ShippingForm
-from payment.models import ShippingAddress
+from django.template.context_processors import request
+from payment.forms import ShippingForm, PaymentForm
+from payment.models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
 
 
 def payment_success(request):
@@ -21,4 +24,57 @@ def checkout(request):
     else:
         shipping_form = ShippingForm(request.POST or None)
         return render(request, 'payment/checkout.html', {"prods": prods, "quantities": quantities, "total": total, "shipping_form":shipping_form})
+
+def billing_info(request):
+    if request.POST:
+        cart = Cart(request)
+        prods = cart.get_products()
+        quantities = cart.get_quantities()
+        total = cart.total()
+
+        shipping_details = request.POST
+        request.session['shipping_details'] = shipping_details
+
+        if request.user.is_authenticated:
+            billing_form = PaymentForm()
+            return render(request, 'payment/billing_info.html',
+                          {"prods": prods, "quantities": quantities, "total": total, "shipping_details": request.POST, "billing_form": billing_form})
+        else:
+            billing_form = PaymentForm()
+            messages.error(request, "You are not logged in!!!")
+            return render(request, 'payment/billing_info.html',
+                          {"prods": prods, "quantities": quantities, "total": total, "shipping_details": request.POST, "billing_form": billing_form})
+
+    else:
+        messages.error(request, "Access denied")
+        return redirect('home')
+
+def process_order(request):
+    if request.POST:
+        cart = Cart(request)
+        total = cart.total()
+
+        payment_form = PaymentForm(request.POST or None)
+
+        ship_details = request.session.get('shipping_details')
+        ship_order_address = f"{ship_details['ship_address1']}\n{ship_details['ship_address2']}\n{ship_details['ship_city']}\n{ship_details['ship_state']}\n{ship_details['ship_country']}\n{ship_details['ship_zipcode']}"
+
+        order_fullname = ship_details['ship_full_name']
+        order_email = ship_details['ship_email']
+        order_amount = total
+
+        if request.user.is_authenticated:
+            order_user = request.user
+            order_details = Order(user=order_user, full_name=order_fullname, email=order_email, address=ship_order_address, amount_paid=order_amount)
+            order_details.save()
+            messages.error(request, "Your order was placed!!!")
+            return redirect('home')
+        else:
+            order_details = Order(full_name=order_fullname, email=order_email, address=ship_order_address, amount_paid=order_amount)
+            order_details.save()
+            messages.error(request, "Your order was placed!!!")
+            return redirect('home')
+    else:
+        messages.error(request, "Access denied!!!")
+        return redirect('home')
 
